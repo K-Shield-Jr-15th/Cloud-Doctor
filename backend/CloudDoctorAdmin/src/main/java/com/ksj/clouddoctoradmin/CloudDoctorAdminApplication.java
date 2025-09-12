6daab99eefda67108fd938835b0f76ce39e49694
DESKTOP-TR03E35\USER <hyoju2259@gmail.com>
2025-09-12 19:13:39 +0900

    feat swagger api 추가

--- backend/CloudDoctorAdmin/src/main/java/com/ksj/clouddoctoradmin/CloudDoctorAdminApplication.java
+++ backend/CloudDoctorAdmin/src/main/java/com/ksj/clouddoctoradmin/CloudDoctorAdminApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudDoctorAdminApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CloudDoctorAdminApplication.class, args);
     }
-
 }
